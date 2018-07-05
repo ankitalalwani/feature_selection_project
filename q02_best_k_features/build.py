eb67afb7ebbeb36230324a572d42c8bc4529548a
ankitalalwani <lalwaniankita20@gmail.com>
2018-07-05 10:26:58 +0000

    Done

--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -1,3 +1,4 @@
+# %load q02_best_k_features/build.py
 # Default imports
 
 import pandas as pd
@@ -9,4 +10,17 @@ from sklearn.feature_selection import f_regression
 
 
 # Write your solution here:
+def percentile_k_features(df,k=20):
+    
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+    
+    X_new = SelectPercentile(f_regression, k).fit_transform(X, y)
+    return X_new.tolist()
+
+percentile_k_features(data)
+
+
+
+
 
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,6 +1,8 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
 import pandas as pd
 import numpy as np
 
@@ -8,3 +10,23 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(df):
+
+    # create a base classifier used to evaluate a subset of attributes
+    X, y = df.iloc[:,:-1], df.iloc[:,-1]
+    
+    model = RandomForestClassifier(random_state=9)
+    model.fit(X, y)
+    
+    sfm = SelectFromModel(model, prefit=True)
+   
+
+    
+
+    return X.iloc[0,sfm.get_support()].index.tolist()
+
+select_from_model(data)
+
+
+
+
