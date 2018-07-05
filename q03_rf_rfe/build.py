1c6e74f98da2c687172b1ce8658b16dea7fb1b81
ankitalalwani <lalwaniankita20@gmail.com>
2018-07-05 10:58:58 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -8,4 +9,23 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+
+def rf_rfe(df):
+    
+    # create a base classifier used to evaluate a subset of attributes
+    model = RandomForestClassifier()
+
+    X, y = df.iloc[:,:-1], df.iloc[:,-1]
+    # create the RFE model and select 3 attributes
+    rfe = RFE(model, 17)
+    rfe = rfe.fit(X, y)
+
+    # summarize the selection of the attributes
+    top_features = X.iloc[0,rfe.ranking_ == 1].index.tolist()
+    return top_features
+
+rf_rfe(data)
+
+
+
 
